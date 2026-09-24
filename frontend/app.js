ce5a3bf1605ebb1c7997bfffd368cd198f6c2b5c
shinbaek25-prog <shinbaek25-prog@gmail.com>
2026-09-25 04:13:56 +0800

    Point frontend to Render API

--- frontend/app.js
+++ frontend/app.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:5000";
+const API_URL = "https://login-system-3-pdif.onrender.com";
 
 const loginForm = document.getElementById("loginForm");
 const registerForm = document.getElementById("registerForm");
